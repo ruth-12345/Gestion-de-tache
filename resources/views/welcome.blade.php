aa891143af97afcb31fd333fbdc1b87f0aee0886
ruth-12345 <155400648+ruth-12345@users.noreply.github.com>
2024-08-08 03:07:05 +0200

    Modification du blade du controller et de la route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-            Bonsoir Ruth comment tu vas 
+            <h1>Bonjour Ruth, il est <?php echo date('H:i:s')?></h1>
 
         </div>
     </body>
