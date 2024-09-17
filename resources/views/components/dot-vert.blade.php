010cf9937b282e4c44cd63ce28c1cd4f83285695
Denis AKPAGNONITE <akpagnonited@gmail.com>
2024-09-17 14:18:00 +0100

    ajout de la logique des 3 boutons

--- resources/views/components/dot-vert.blade.php
+++ resources/views/components/dot-vert.blade.php
@@ -39,10 +39,27 @@
         class="absolute top-11 left-0 flex w-full min-w-[12rem] flex-col overflow-hidden rounded-xl border border-slate-300 bg-slate-100 py-1.5"
         role="menu"
     >
-        <a href="#" class="bg-slate-100 px-4 py-2 text-sm text-slate-700 hover:bg-slate-800/5 hover:text-black focus-visible:bg-slate-800/10 focus-visible:text-black focus-visible:outline-none" role="menuitem">Terminer</a>
+        <a href="{{ route('task.status', $task) }}"
+           class="bg-slate-100 px-4 py-2 text-sm text-slate-700 hover:bg-slate-800/5 hover:text-black focus-visible:bg-slate-800/10 focus-visible:text-black focus-visible:outline-none
+          {{ $task->status == 2 ? 'disabled opacity-50 cursor-not-allowed' : '' }}"
+           @if($task->status == 2) aria-disabled="true" @endif
+           role="menuitem">
+            Terminer
+        </a>
         <a @click.prevent="copyToClipboard" id="copyFullPath" href="{{ url()->full() }}/{{$task->id}}"
            class="bg-slate-100 px-4 py-2 text-sm text-slate-700 hover:bg-slate-800/5 hover:text-black focus-visible:bg-slate-800/10 focus-visible:text-black focus-visible:outline-none"
            role="menuitem">Copier le lien</a>
-        <a href="#" class="bg-slate-100 px-4 py-2 text-sm text-slate-700 hover:bg-slate-800/5 hover:text-black focus-visible:bg-slate-800/10 focus-visible:text-black focus-visible:outline-none" role="menuitem">Supprimer</a>
+        <a onclick="event.preventDefault(); document.getElementById('delete-form-{{ $task->id }}').submit();" href="#"
+           class="bg-slate-100 px-4 py-2 text-sm text-slate-700 hover:bg-slate-800/5 hover:text-black focus-visible:bg-slate-800/10 focus-visible:text-black focus-visible:outline-none"
+           role="menuitem">
+            Supprimer
+
+            <form id="delete-form-{{ $task->id }}" action="{{ route('task.destroy', $task) }}" method="POST"
+                  style="display: none;">
+                @csrf
+                @method('DELETE')
+            </form>
+
+        </a>
     </div>
 </div>
